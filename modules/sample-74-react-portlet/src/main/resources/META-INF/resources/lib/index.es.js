266ee22b72685b4489445e93df45f694c17bbcba
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2021-08-12 10:19:59 +0200

    Added sample with Liferay Companies request

--- modules/sample-74-react-portlet/src/main/resources/META-INF/resources/lib/index.es.js
+++ modules/sample-74-react-portlet/src/main/resources/META-INF/resources/lib/index.es.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-// import ClayButton from '@clayui/button';
 import ClayDatePicker from '@clayui/date-picker';
 
 const spritemap = themeDisplay.getPathThemeImages() + '/lexicon/icons.svg';
 
 const ClayDatePickerComponent = () => {
-    const [value, setValue] = useState(null);
+    const [value, setValue] = useState("2021-08-12");
 
     return (
         <ClayDatePicker
@@ -23,36 +22,32 @@ const ClayDatePickerComponent = () => {
 };
 
 const Application = (props) => {
-    // const [companies, setCompanies] = useState([]);
+    const [companies, setCompanies] = useState([]);
 
-    // useEffect(() => {
-    //     Liferay.Service(
-    //         '/company/get-companies',
-    //         (data) => setCompanies(data)
-    //     );
-    // }, []);
+    useEffect(() => {
+        Liferay.Service(
+            '/company/get-companies',
+            (data) => setCompanies(data)
+        );
+    }, []);
 
     return <div>
         <h1>Hello from functional component</h1>
 
-        {/*{companies.length > 0 &&*/}
-        {/*<div>*/}
-        {/*    <h2>Companies</h2>*/}
-        {/*    <ul>*/}
-        {/*        {companies.map((company) => (*/}
-        {/*            <li>{company.webId}, {company.companyId}</li>*/}
-        {/*        ))}*/}
-        {/*    </ul>*/}
-        {/*</div>*/}
-        {/*}*/}
-
-        {/*<ClayButton displayType="primary">*/}
-        {/*    Button*/}
-        {/*</ClayButton>*/}
+        {companies.length > 0 &&
+        <div>
+            <h2>Companies</h2>
+            <ul>
+                {companies.map((company) => (
+                    <li>{company.webId}, {company.companyId}</li>
+                ))}
+            </ul>
+        </div>
+        }
 
         <h1>Is the ClayDatePicker coming up here?</h1>
 
-        <ClayDatePickerComponent />
+        <ClayDatePickerComponent/>
     </div>;
 }
 
